1f5d20b51f64d3ea3a17eb849ac2d2561774e75c
Vunb <nhubaovu@gmail.com>
2021-02-19 09:30:55 +0700

    Add CTA, Chatbox

---
+++ docs/chatbox.js
@@ -0,0 +1,62 @@
+$(function () {
+  var INDEX = 0;
+  $("#chat-submit").click(function (e) {
+    e.preventDefault();
+    var msg = $("#chat-input").val();
+    if (msg.trim() == '') {
+      return false;
+    }
+    generate_message(msg, 'self');
+    var buttons = [
+      {
+        name: 'Existing User',
+        value: 'existing'
+      },
+      {
+        name: 'New User',
+        value: 'new'
+      }
+    ];
+    setTimeout(function () {
+      generate_message(msg, 'user');
+    }, 1000)
+
+  })
+
+  function generate_message(msg, type) {
+    INDEX++;
+    var str = "";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg " + type + "\">";
+    str += "          <span class=\"msg-avatar\">";
+    str += `            <img src="user-profile.png">`;
+    str += "          <\/span>";
+    str += "          <div class=\"cm-msg-text\">";
+    str += msg;
+    str += "          <\/div>";
+    str += "        <\/div>";
+    $(".chat-logs").append(str);
+    $("#cm-msg-" + INDEX).hide().fadeIn(300);
+    if (type == 'self') {
+      $("#chat-input").val('');
+    }
+    $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
+  }
+
+  $(document).delegate(".chat-btn", "click", function () {
+    var value = $(this).attr("chat-value");
+    var name = $(this).html();
+    $("#chat-input").attr("disabled", false);
+    generate_message(name, 'self');
+  })
+
+  $("#chat-circle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+
+  $(".chat-box-toggle").click(function () {
+    $("#chat-circle").toggle('scale');
+    $(".chat-box").toggle('scale');
+  })
+
+})
