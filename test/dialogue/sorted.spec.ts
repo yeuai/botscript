719ae09966e32c7a80822c5585670b3963e78da5
Vunb <nhubaovu@gmail.com>
2021-06-05 19:53:30 +0700

    add sorted spec test

---
+++ test/dialogue/sorted.spec.ts
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+import { BotScript, Request } from '../../src/engine';
+
+describe('Dialogue: sorted activator before reply', () => {
+  // TODO: implement
+  assert.fail('Not implemented!');
+})
